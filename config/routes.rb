2d5655984caaccfd90df5fb1e7c538fc59058505
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 16:07:40 +0000

    Integrated skills via model generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
+  
+  get 'home', to: 'blogs#home'
+  get 'about', to: 'blogs#about'
+  get 'contact', to: 'blogs#contact'
+
+
 end
